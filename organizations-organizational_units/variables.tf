7b9c6e0b8294df2572812466364cacd2ba5e1dea
steenkamby <steenkamby@live.dk>
2020-07-02 18:17:00 +0200

    accounts and OUs

---
+++ organizations-organizational_units/variables.tf
@@ -0,0 +1,6 @@
+variable "name" {
+  description = "After the Terraform docs: 'The name for the organizational unit'"
+}
+variable "parent_id" {
+  description = "After the Terraform docs: 'ID of the parent organizational unit, which may be the root'"
+}
